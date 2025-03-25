3a24a684d05e8a283c00201767bc137d36aaf01c
Joshua Talks <joshua.talks@embl.de>
2025-03-25 14:27:15 +0100

    test yaml generator function

---
+++ scripts/test_scripts/yaml_generator_test.py
@@ -0,0 +1,9 @@
+from model_ranking.yaml_generators import generate_yaml
+
+# from model_ranking.dataclass import Model3LayerSourceConfig
+from pytorch3dunet.unet3d.config import load_config_direct  # type: ignore
+
+config_path = "/g/kreshuk/talks/model_ranking/scripts/test_scripts/test_config.yaml"
+
+yaml_paths = generate_yaml(config_path)
+print(yaml_paths)
