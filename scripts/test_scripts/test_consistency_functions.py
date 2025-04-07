3734d7c5b33c96d64cbc525c00c60d5f56edf7f8
Joshua Talks <joshua.talks@embl.de>
2025-04-07 10:10:16 +0200

    check consistency functions

--- scripts/test_scripts/test_consistency_functions.py
+++ scripts/test_scripts/test_consistency_functions.py
@@ -1,40 +1,24 @@
 from model_ranking.consistency import (
-    get_consistency_loaders,
-    calc_consistency_score,
+    # get_consistency_loaders,
+    # calc_consistency_score,
+    run_consistency_evaluation,
 )
 from model_ranking.dataclass import (
     AdaptedRandErrorConfig,
     ConsistencyConfig,
     ConsistencyMetaConfig,
-    Eval_TIF_TxtDataloaderMetaConfig,
+    # Eval_TIF_TxtDataloaderMetaConfig,
+    BBBC039TargetConfig,
 )
 
-consis_dataloader = Eval_TIF_TxtDataloaderMetaConfig(
-    batch_size=1,
-    num_workers=8,
-    name="TIF_txt_Dataset",
-    expand_dims=True,
-    global_norm=False,
-    percentiles=None,
-    image_key="segmentation",
-    min_object_size=50,
-    instance_zero_background=False,
-    mask_dir=(
-        "/g/kreshuk/talks/model_ranking/scripts/test_scripts/BBBC039_to_BBBC039_gap/feature_perturbation_consistency/test/BC_model4/norm_50_980/none/predictions",
-    ),
-    mask_key="segmentation",
-    filenames_path="/BBBC039/test.txt",
-    transformer={
-        "raw": [],
-        "label": [
-            {"name": "CropToFixed", "size": (512, 512), "centered": True},
-        ],
-    },
-)
-
-dataloader_config = consis_dataloader.create_config(
-    image_dir=(
+dataloader_config = BBBC039TargetConfig.model_fields[
+    "consis_dataloader_instance"
+].default.create_consis_config(
+    perturbed_dir=(
         "/g/kreshuk/talks/model_ranking/scripts/test_scripts/BBBC039_to_BBBC039_gap/feature_perturbation_consistency/test/BC_model4/norm_50_980/DO_a01/predictions",
+    ),
+    unperturbed_dir=(
+        "/g/kreshuk/talks/model_ranking/scripts/test_scripts/BBBC039_to_BBBC039_gap/feature_perturbation_consistency/test/BC_model4/norm_50_980/none/predictions",
     ),
     data_base_path="/scratch/talks/data",
 )
@@ -50,8 +34,12 @@ consis_config = ConsistencyConfig(
     ),
 )
 
+scores, masks = run_consistency_evaluation(consis_config)
+
+"""
 for loader in get_consistency_loaders(dataloader_config):
     scores, mask = calc_consistency_score(
         loader,
         consis_config,
     )
+"""
