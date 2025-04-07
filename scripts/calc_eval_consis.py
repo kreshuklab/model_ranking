871fff743dbccc2bfd43d7b7b9ec2912eb6abb7d
Joshua Talks <joshua.talks@embl.de>
2025-04-07 15:16:40 +0200

    sript to evaluate and calculate consistency for specific prediction, single config yaml

---
+++ scripts/calc_eval_consis.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+import typer
+from pathlib import Path
+
+from pytorch3dunet.unet3d.config import (
+    load_config_direct,  # pyright: ignore[reportUnknownVariableType]
+)
+
+from model_ranking.consistency import (
+    run_consistency_evaluation,
+)
+from model_ranking.dataclass import EvaluateConfig, ConsistencyConfig
+from model_ranking.evaluation import run_evaluation
+
+
+def main(
+    config: Annotated[str, typer.Option(help="Path to the config file", exists=True)],
+):
+    cfg, _ = load_config_direct(config)
+    eval_config = EvaluateConfig.model_validate(cfg["evaluation"])
+    _ = run_evaluation(eval_config)
+
+    if "none" in str(Path(config).name):
+        print(f"Skipping consistency evaluation for {Path(config).stem}")
+    else:
+        consis_config = ConsistencyConfig.model_validate(cfg["consistency"])
+        _, _ = run_consistency_evaluation(consis_config)
+
+
+if __name__ == "__main__":
+    typer.run(main)
