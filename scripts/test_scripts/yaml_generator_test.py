9354912fb70431d70bc0376b14dfee72e1294d2d
Joshua Talks <joshua.talks@embl.de>
2025-04-07 11:15:28 +0200

    run test

--- scripts/test_scripts/yaml_generator_test.py
+++ scripts/test_scripts/yaml_generator_test.py
@@ -3,7 +3,7 @@ from model_ranking.yaml_generators import generate_yaml
 # from model_ranking.dataclass import Model3LayerSourceConfig
 from pytorch3dunet.unet3d.config import load_config_direct  # type: ignore
 
-config_path = "/g/kreshuk/talks/model_ranking/scripts/test_scripts/test_config.yaml"
+config_path = "/g/kreshuk/talks/model_ranking/scripts/test_scripts/test_config3.yaml"
 
 yaml_paths = generate_yaml(config_path)
 print(yaml_paths)
