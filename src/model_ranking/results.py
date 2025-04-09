7b6e9cc32c028f9d9455a43e3f1a71d91033650c
Joshua Talks <joshua.talks@embl.de>
2025-04-09 10:42:07 +0200

    load consistency results

--- src/model_ranking/results.py
+++ src/model_ranking/results.py
@@ -1,9 +1,10 @@
-from typing import Dict, List, Optional, Union, Any, Sequence
+from typing import Dict, List, Optional, Union, Any, Sequence, Tuple
 from pathlib import Path
 import h5py  # pyright: ignore[reportMissingTypeStubs]
 import numpy as np
 from numpy.typing import NDArray
 import imageio.v2 as imageio
+from tqdm import tqdm
 
 from model_ranking.dataclass import (
     ForegroundFilterConfig,
@@ -19,6 +20,7 @@ from model_ranking.utils import (
     is_ndarray,
     load_select_prediction_scores,
     create_h5_dataset,
+    get_output_dir,
 )
 
 
@@ -240,3 +242,238 @@ def save_summary_metrics(
                 consis_std_per_alpha,
                 config.overwrite_scores,
             )
+
+
+def get_consis_results(
+    source_data: List[str],
+    target_data: List[str],
+    selected_augmentations: Dict[str, List[str]],
+    consis_keys: Dict[str, str],
+    result_folders: Dict[str, str],
+    source_models: Dict[str, str] = {
+        "BBBC039": "BC_model4",
+        "DSB2018": "DSB_model4",
+        "Go-Nuclear": "GN_model4",
+        "HeLaNuc": "HN_model1",
+        "Hoechst": "Hst_model5",
+        "S_BIAD634": "634_model1",
+        "S_BIAD895": "895_model2",
+        "S_BIAD1196": "1196_model3",
+        "S_BIAD1410": "1410_model2",
+        "FlyWing": "fw_model8",
+        "Ovules": "ov_model8",
+        "PNAS": "p_model5",
+        "EPFL": "E_model4",
+        "Hmito": "Hm_model3",
+        "Rmito": "Rm_model3",
+    },
+    selected_norms: Dict[str, List[Optional[Tuple[float, float]]]] = {
+        "BBBC039": [(5, 98)],
+        "DSB2018": [(5, 98)],
+        "Go-Nuclear": [(0, 99.8)],
+        "HeLaNuc": [(5, 99.6)],
+        "Hoechst": [(5, 98)],
+        "S_BIAD634": [(5, 98)],
+        "S_BIAD895": [(5, 98)],
+        "S_BIAD1196": [(5, 98)],
+        "S_BIAD1410": [(5, 98)],
+        "FlyWing": [(5, 95)],
+        "Ovules": [(5, 95)],
+        "PNAS": [(5, 95)],
+        "EPFL": [None],
+        "Hmito": [None],
+        "Rmito": [None],
+        "VNC": [None],
+    },
+    perf_key: str = "hard_f1",
+    model_abbreviations: Dict[str, str] = {
+        "BBBC039": "BC",
+        "DSB2018": "DSB",
+        "Go-Nuclear": "GN",
+        "HeLaNuc": "HN",
+        "Hoechst": "Hst",
+        "S_BIAD634": "634",
+        "S_BIAD895": "895",
+        "S_BIAD1196": "1196",
+        "S_BIAD1410": "1410",
+        "FlyWing": "fw",
+        "Ovules": "ov",
+        "PNAS": "p",
+        "EPFL": "E",
+        "Hmito": "Hm",
+        "Rmito": "Rm",
+        "VNC": "V",
+        "affable-shark": "AS",
+        "cp_nuclei": "CN",
+        "cp_cyto3": "C3",
+        "root_nuclei_ds1x": "RN",
+        "laid-back-lobster": "LL",
+        "pioneering-rhino": "PR",
+    },
+    select_results_by_source: bool = False,
+    approach: str = "consistency",
+    per_transfer_norms: bool = False,
+    per_target_norms: bool = True,
+    consis_postfix: str = "mean",
+    perf_postfix: str = "mean",
+    base_seg_dir: str = "/g/kreshuk/talks/domain_gap/experiments/patch_segmentation",
+):
+
+    consis_PT_PA_type: Dict[str, Dict[str, Dict[str, NDArray[Any]]]] = {}
+    consis_PT_PA_strength: Dict[str, Dict[str, Dict[str, NDArray[Any]]]] = {}
+    consis_std_PT_PA_strength: Dict[str, Dict[str, Dict[str, NDArray[Any]]]] = {}
+    perf_PT_PA_type: Dict[str, Dict[str, Dict[str, NDArray[Any]]]] = {}
+    perf_PT_PA_strength: Dict[str, Dict[str, Dict[str, NDArray[Any]]]] = {}
+    no_aug_perf_scores: Dict[str, Dict[str, float]] = {}
+    no_aug_perf_std: Dict[str, Dict[str, float]] = {}
+    for source in source_data:
+        print(f"Source: {source}")
+        for target in tqdm(target_data):
+            transfer = (
+                model_abbreviations[source] + "_to_" + model_abbreviations[target]
+            )
+            if select_results_by_source:
+                result_folder = result_folders[source]
+            else:
+                result_folder = result_folders[target]
+            output_dir = get_output_dir(
+                source=source,
+                target=target,
+                model_name=source_models[source],
+                output_folder=None,
+                result_type=result_folder,
+                approach=approach,
+                base_seg_folder=base_seg_dir,
+            )
+            consis_PT_PN_PA_type: Dict[str, Dict[str, NDArray[Any]]] = {}
+            consis_std_PT_PN_PA_strength: Dict[str, Dict[str, NDArray[Any]]] = {}
+            consis_PT_PN_PA_strength: Dict[str, Dict[str, NDArray[Any]]] = {}
+            perf_PT_PN_PA_type: Dict[str, Dict[str, NDArray[Any]]] = {}
+            perf_PT_PN_PA_strength: Dict[str, Dict[str, NDArray[Any]]] = {}
+            no_aug_PN_perf_scores: Dict[str, float] = {}
+            no_aug_PN_perf_std: Dict[str, float] = {}
+            if per_transfer_norms:
+                norms = selected_norms[transfer]
+            elif per_target_norms:
+                norms = selected_norms[target]
+            else:
+                norms = selected_norms
+            for norm in norms:
+                if norm == None:
+                    norm_foldername = "norm_Normalize"
+                else:
+                    norm_foldername = (
+                        f"norm_{str(norm[0])}_{str(norm[1]).replace('.', '')}"
+                    )
+                norm_dir_path = Path(output_dir) / norm_foldername
+
+                consis_per_aug_strength: Dict[str, NDArray[Any]] = {}
+                consis_std_per_aug_strength: Dict[str, NDArray[Any]] = {}
+                consis_per_aug_type: Dict[str, NDArray[Any]] = {}
+                perf_per_aug_strength: Dict[str, NDArray[Any]] = {}
+                perf_per_aug_type: Dict[str, NDArray[Any]] = {}
+
+                for aug, alphas in selected_augmentations.items():
+                    if aug == "none":
+                        metric_filepath = (
+                            Path(norm_dir_path) / f"{aug}" / "metric_summary.h5"
+                        )
+                        with h5py.File(metric_filepath, "r") as f:
+                            ds = f[f"{perf_key}"]
+                            assert isinstance(ds, h5py.Dataset)
+                            perf_per_patch = (# pyright: ignore[reportUnknownVariableType]
+                                ds[...]
+                            ) 
+                            assert is_ndarray(
+                                perf_per_patch
+                            ), "perf_per_patch must be a numpy array"
+                            if perf_per_patch.ndim == 2:
+                                perf_std = np.nanstd(perf_per_patch, axis=0)[0]
+                            else:
+                                perf_std = np.nanstd(perf_per_patch, axis=0)
+                            ds = f[f"{perf_key}_{perf_postfix}"]
+                            assert isinstance(ds, h5py.Dataset)
+                            perf_score = ds[...] # pyright: ignore[reportUnknownVariableType]
+                            assert is_ndarray(
+                                perf_score
+                            ), "perf_score must be a numpy array"
+                            if perf_score.size == 2:
+                                perf_score = perf_score[1]
+                            elif perf_score.size == 3:
+                                perf_score = perf_score[0]
+                            elif (len(perf_score.shape) == 1) and (
+                                perf_score.size == 0
+                            ):
+                                perf_score = perf_score[0]
+                            else:
+                                raise ValueError(
+                                    f"perf_score has unexpected shape {perf_score.shape}"
+                                )
+                            assert isinstance(perf_score, float), "perf_score must be a float"
+                            assert isinstance(perf_std, float), "perf_std must be a float"
+                            no_aug_PN_perf_scores[norm_foldername] = perf_score
+                            no_aug_PN_perf_std[norm_foldername] = perf_std
+                    else:
+                        consis_per_alpha = np.zeros(len(alphas))
+                        consis_std_per_alpha = np.zeros(len(alphas))
+                        perf_per_alpha = np.zeros(len(alphas))
+                        for i, alpha in enumerate(alphas):
+                            metric_filepath = (
+                                Path(norm_dir_path)
+                                / f"{aug}_{alpha}"
+                                / "metric_summary.h5"
+                            )
+                            with h5py.File(metric_filepath, "r") as f:
+                                ds = f[f"{consis_keys[target]}_{consis_postfix}"]
+                                assert isinstance(ds, h5py.Dataset), f"{consis_keys[target]}_{consis_postfix} must be a h5py.Dataset"
+                                consis_per_alpha[i] = ds[...]
+                                ds = f[f"{consis_keys[target]}_per_patch"]
+                                assert isinstance(ds, h5py.Dataset), f"{consis_keys[target]}_per_patch must be a h5py.Dataset"
+                                consis_std_per_alpha[i] = np.nanstd(
+                                    ds[...] # pyright: ignore[reportUnknownArgumentType]
+                                )
+                                ds = f[f"{perf_key}_{perf_postfix}"]
+                                assert isinstance(ds, h5py.Dataset), f"{perf_key}_{perf_postfix} must be a h5py.Dataset"
+                                perf_score = ds[...] # pyright: ignore[reportUnknownVariableType]
+                                assert is_ndarray(
+                                    perf_score
+                                ), "perf_score must be a numpy array"
+                                if perf_score.size == 2:
+                                    perf_score = perf_score[1]
+                                elif perf_score.size == 3:
+                                    perf_score = perf_score[0]
+                                elif (len(perf_score.shape) == 1) and (
+                                    perf_score.size == 0
+                                ):
+                                    perf_score = perf_score[0]
+                                perf_per_alpha[i] = perf_score
+                        consis_per_aug_strength[aug] = consis_per_alpha
+                        consis_std_per_aug_strength[aug] = consis_std_per_alpha
+                        consis_per_aug_type[aug] = np.array(
+                            [np.nanmean(consis_per_alpha)]
+                        )
+                        perf_per_aug_strength[aug] = perf_per_alpha
+                        perf_per_aug_type[aug] = np.array([np.nanmean(perf_per_alpha)])
+                consis_PT_PN_PA_strength[norm_foldername] = consis_per_aug_strength
+                consis_std_PT_PN_PA_strength[norm_foldername] = (
+                    consis_std_per_aug_strength
+                )
+                consis_PT_PN_PA_type[norm_foldername] = consis_per_aug_type
+                perf_PT_PN_PA_strength[norm_foldername] = perf_per_aug_strength
+                perf_PT_PN_PA_type[norm_foldername] = perf_per_aug_type
+            consis_PT_PA_strength[transfer] = consis_PT_PN_PA_strength
+            consis_std_PT_PA_strength[transfer] = consis_std_PT_PN_PA_strength
+            consis_PT_PA_type[transfer] = consis_PT_PN_PA_type
+            perf_PT_PA_strength[transfer] = perf_PT_PN_PA_strength
+            perf_PT_PA_type[transfer] = perf_PT_PN_PA_type
+            no_aug_perf_scores[transfer] = no_aug_PN_perf_scores
+            no_aug_perf_std[transfer] = no_aug_PN_perf_std
+    return (
+        consis_PT_PA_type,
+        consis_PT_PA_strength,
+        consis_std_PT_PA_strength,
+        perf_PT_PA_type,
+        perf_PT_PA_strength,
+        no_aug_perf_scores,
+        no_aug_perf_std,
+    )
