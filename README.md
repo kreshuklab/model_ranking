d961737dd62c2093e39f17eab2e0169d8c58ecc2
Joshua Talks <joshua.talks@embl.de>
2025-04-11 14:26:27 +0200

    delete files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CRank - Consistency based Ranking
-### Ranking pre-trained segmentation models for zero-shot transferability [link to paper](https://arxiv.org/abs/2503.00450)
-
+### Ranking pre-trained segmentation models for zero-shot transferability 
+([link to paper](https://arxiv.org/abs/2503.00450))
 
 ![Fig1](MICCAI_intro_figure.svg)
+
+
--- scripts/test_scripts/yaml_generator_test.py
+++
@@ -1,9 +0,0 @@
-from model_ranking.yaml_generators import generate_run_yamls
-
-# from model_ranking.dataclass import Model3LayerSourceConfig
-from pytorch3dunet.unet3d.config import load_config_direct  # type: ignore
-
-config_path = "/g/kreshuk/talks/model_ranking/scripts/test_scripts/test_config3.yaml"
-
-yaml_paths = generate_run_yamls(config_path)
-print(yaml_paths)
