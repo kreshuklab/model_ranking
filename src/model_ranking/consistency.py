625c568cdaaef02490f00f0217766495451c7d39
Joshua Talks <joshua.talks@embl.de>
2025-05-16 11:01:43 +0200

    ensure type set

--- src/model_ranking/consistency.py
+++ src/model_ranking/consistency.py
@@ -16,7 +16,6 @@ from model_ranking.dataclass import (
     TIFEvalDatasetConfig,
 )
 from model_ranking.datasets import StandardEvalDataset
-
 from model_ranking.metrics import (
     AdaptedRandErrorEval,
     get_mask,
@@ -24,6 +23,7 @@ from model_ranking.metrics import (
 from model_ranking.utils import (
     save_h5,
     loader_classes,
+    is_ndarray,
 )
 
 
@@ -89,8 +89,14 @@ def calc_consistency_score(
             assert isinstance(
                 unperturbed_pred, torch.Tensor
             ), "unperturbed_pred is not a torch.Tensor"
-            perturbed_pred: NDArray[Any] = perturbed_pred.numpy()
-            unperturbed_pred: NDArray[Any] = unperturbed_pred.numpy()
+            perturbed_pred = (  # pyright: ignore[reportUnknownVariableType]
+                perturbed_pred.numpy()
+            )
+            unperturbed_pred = (  # pyright: ignore[reportUnknownVariableType]
+                unperturbed_pred.numpy()
+            )
+            assert is_ndarray(perturbed_pred), "perturbed_pred is not a numpy array"
+            assert is_ndarray(unperturbed_pred), "unperturbed_pred is not a numpy array"
         if isinstance(metric, AdaptedRandErrorEval):
             batch_scores, batch_consis_mask = metric(perturbed_pred, unperturbed_pred)
 
