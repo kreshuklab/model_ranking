85134ca7d680d0aa06771e7462fd97ebdc4639ac
Joshua Talks <joshua.talks@embl.de>
2025-05-23 09:32:20 +0200

    add pytright ignores

--- src/model_ranking/metrics.py
+++ src/model_ranking/metrics.py
@@ -14,7 +14,7 @@ from skimage.morphology import (
 from skimage.metrics import (
     adapted_rand_error,  # pyright: ignore[reportUnknownVariableType]
 )
-from pytorch3dunet.unet3d.metrics import (
+from pytorch3dunet.unet3d.metrics import (  # pyright: ignore[reportMissingTypeStubs]
     DiceCoefficient,
 )
 from model_ranking.utils import is_ndarray, avoid_int_overflow
@@ -296,9 +296,9 @@ class CrossEntropyEval:
                 self.eps,
                 1 - self.eps,
             )
-            metric_result_pp = entropy(probs_NA, base=self.entr_base) + entropy(
-                probs_NA, probs_A, base=self.entr_base
-            )
+            metric_result_pp = entropy(  # pyright: ignore[reportUnknownVariableType]
+                probs_NA, base=self.entr_base
+            ) + entropy(probs_NA, probs_A, base=self.entr_base)
             assert is_ndarray(
                 metric_result_pp
             ), f"Data is not a numpy array: {metric_result_pp}"
