94cfe48e6dcc5f09695a5decf842f14f64bc6f6c
Joshua Talks <joshua.talks@embl.de>
2025-06-04 18:11:46 +0200

    bugfix to ensure correct copy location

--- src/model_ranking/config.py
+++ src/model_ranking/config.py
@@ -8,7 +8,7 @@ from model_ranking.dataclass import MeanTeacherConfig
 def copy_config(config: MeanTeacherConfig, config_path: Union[str, Path]):
     if not isinstance(config_path, Path):
         config_path = Path(config_path)
-    checkpoint_path = config_path.parent / "checkpoints"
+    checkpoint_path = Path(config.output_root_path) / "checkpoints"
     assert checkpoint_path.exists(), f"Checkpoint path {checkpoint_path} does not exist"
     new_config_path = checkpoint_path / config.name / config_path.name
     new_config_path.parent.mkdir(parents=True, exist_ok=True)
