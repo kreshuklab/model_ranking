d61cc6581147c0caddb4c9c3d970cc2966f47082
Joshua Talks <joshua.talks@embl.de>
2025-06-04 18:10:36 +0200

    combine training and prediction into single script for batch processing

--- scripts/mean_teacher_finetune.py
+++ scripts/mean_teacher_finetune.py
@@ -9,36 +9,42 @@ from pytorch3dunet.unet3d.config import (
 )
 
 
+def self_training_mean_teacher(
+    mean_teacher_config: MeanTeacherConfig, config_path: str
+):
+    run_mean_teacher(
+        name=mean_teacher_config.name,
+        output_root_path=mean_teacher_config.output_root_path,
+        unsupervised_train_paths=mean_teacher_config.data_cfg.unsupervised_train_paths,
+        unsupervised_val_paths=mean_teacher_config.data_cfg.unsupervised_val_paths,
+        patch_shape=mean_teacher_config.data_cfg.patch_shape,
+        pseudo_labeler_config=mean_teacher_config.pseudo_labeler_cfg,
+        model_config=mean_teacher_config.model_cfg.model,
+        source_checkpoint=mean_teacher_config.model_cfg.source_checkpoint,
+        supervised_train_paths=mean_teacher_config.data_cfg.supervised_train_paths,
+        supervised_val_paths=mean_teacher_config.data_cfg.supervised_val_paths,
+        raw_key=mean_teacher_config.data_cfg.raw_key,
+        raw_key_supervised=mean_teacher_config.data_cfg.raw_key_supervised,
+        label_key=mean_teacher_config.data_cfg.label_key,
+        batch_size=mean_teacher_config.data_cfg.batch_size,
+        lr=mean_teacher_config.training_cfg.lr,
+        n_iterations=mean_teacher_config.training_cfg.n_iterations,
+        epochs=mean_teacher_config.training_cfg.epochs,
+        n_samples_train=mean_teacher_config.data_cfg.n_samples_train,
+        n_samples_val=mean_teacher_config.data_cfg.n_samples_val,
+        save_ckpt_every_kth_epoch=mean_teacher_config.training_cfg.save_ckpt_every_kth_epoch,
+        wandb_config=mean_teacher_config.wandb_cfg,
+    )
+    copy_config(mean_teacher_config, config_path)
+
+
 def main(
     config: Annotated[str, typer.Option(help="Path to config file", exists=True)],
 ):
     cfg, _ = load_config_direct(config)
     mt_cfg = MeanTeacherConfig.model_validate(cfg)
 
-    run_mean_teacher(
-        name=mt_cfg.name,
-        output_root_path=mt_cfg.output_root_path,
-        unsupervised_train_paths=mt_cfg.data_cfg.unsupervised_train_paths,
-        unsupervised_val_paths=mt_cfg.data_cfg.unsupervised_val_paths,
-        patch_shape=mt_cfg.data_cfg.patch_shape,
-        pseudo_labeler_config=mt_cfg.pseudo_labeler_cfg,
-        model_config=mt_cfg.model_cfg.model,
-        source_checkpoint=mt_cfg.model_cfg.source_checkpoint,
-        supervised_train_paths=mt_cfg.data_cfg.supervised_train_paths,
-        supervised_val_paths=mt_cfg.data_cfg.supervised_val_paths,
-        raw_key=mt_cfg.data_cfg.raw_key,
-        raw_key_supervised=mt_cfg.data_cfg.raw_key_supervised,
-        label_key=mt_cfg.data_cfg.label_key,
-        batch_size=mt_cfg.data_cfg.batch_size,
-        lr=mt_cfg.training_cfg.lr,
-        n_iterations=mt_cfg.training_cfg.n_iterations,
-        epochs=mt_cfg.training_cfg.epochs,
-        n_samples_train=mt_cfg.data_cfg.n_samples_train,
-        n_samples_val=mt_cfg.data_cfg.n_samples_val,
-        save_ckpt_every_kth_epoch=mt_cfg.training_cfg.save_ckpt_every_kth_epoch,
-        wandb_config=mt_cfg.wandb_cfg,
-    )
-    copy_config(mt_cfg, config)
+    self_training_mean_teacher(mt_cfg, config)
 
 
 if __name__ == "__main__":
