6e508a8e8e275ba34a6977b8f7b7de5f51383c21
Joshua Talks <joshua.talks@embl.de>
2025-07-31 17:31:08 +0200

    script to copy directories from scratch

---
+++ scripts/copy_from_scratch.py
@@ -0,0 +1,13 @@
+from tqdm import tqdm
+from pathlib import Path
+import shutil
+
+scratch_base = Path("/scratch/talks")
+g_base = Path("/g/kreshuk/talks/")
+dir_path = Path("consistency_results/patch_segmentation/mitochondria")
+combined_path = scratch_base / dir_path
+
+paths = list(combined_path.rglob("**/FP_full"))
+for path in tqdm(paths):
+    g_path = Path(str(path).replace(str(scratch_base), str(g_base)))
+    _ = shutil.copytree(path, g_path, dirs_exist_ok=True)
