b2fc07a9c7be34ac236363a3a809547054e4e47e
Joshua Talks <joshua.talks@embl.de>
2025-08-08 16:17:50 +0200

    PCS reduction of features

--- src/model_ranking/GBC.py
+++ src/model_ranking/GBC.py
@@ -1,8 +1,10 @@
 import numpy as np
 from numpy.typing import NDArray
 
-# from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA
 from typing import Dict, Any, Literal, List, Union
+
+from model_ranking.utils import is_ndarray
 
 
 def compute_bhattacharyya_distance(
@@ -59,6 +61,7 @@ def bhattacharyya_coefficient(
     features: NDArray[Any],
     target_labels: NDArray[Any],
     gaussian_type: Literal["diagonal", "spherical"] = "diagonal",
+    n_feature_components: int = 16,
 ):
     """Compute Gaussian Bhattacharyya Coefficient (GBC).
       Args:
@@ -72,21 +75,21 @@ def bhattacharyya_coefficient(
 
     assert gaussian_type in ("diagonal", "spherical")
 
-    # first calculate pca. We take 64 components according to paper's ablations
-    # but limit to the actual number of features if less than 64
-    # n_components = min(64, features.shape[1])
-    # features_pca = PCA(n_components=n_components, random_state=42).fit_transform(
-    #     features
-    # )
+    n_components = min(n_feature_components, features.shape[1])
+    features_pca = PCA(  # pyright: ignore[reportUnknownVariableType]
+        n_components=n_components, random_state=42
+    ).fit_transform(features)
+
+    assert is_ndarray(features_pca), f"Expected features_pca to be a numpy array"
 
     unique_labels = np.unique(target_labels)
     unique_labels = list(unique_labels)
-    # per_class_stats = compute_per_class_mean_and_variance(
-    #     features_pca, target_labels, unique_labels
-    # )
     per_class_stats = compute_per_class_mean_and_variance(
-        features, target_labels, unique_labels
+        features_pca, target_labels, unique_labels
     )
+    # per_class_stats = compute_per_class_mean_and_variance(
+    #     features, target_labels, unique_labels
+    # )
 
     per_class_bhattacharyya_distance: List[float] = []
     for c1 in unique_labels:
