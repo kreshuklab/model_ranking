78541af5b265dd820f54a55f73e8fb3599691cfd
Joshua Talks <joshua.talks@embl.de>
2025-08-12 18:06:05 +0200

    run script

--- scripts/copy_from_scratch.py
+++ scripts/copy_from_scratch.py
@@ -7,7 +7,9 @@ g_base = Path("/g/kreshuk/talks/")
 dir_path = Path("consistency_results/patch_segmentation/mitochondria")
 combined_path = scratch_base / dir_path
 
-paths = list(combined_path.rglob("**/FP_full"))
+paths = list(combined_path.rglob("**/P_full"))
 for path in tqdm(paths):
+    # print(path)
     g_path = Path(str(path).replace(str(scratch_base), str(g_base)))
+    # print(g_path)
     _ = shutil.copytree(path, g_path, dirs_exist_ok=True)
