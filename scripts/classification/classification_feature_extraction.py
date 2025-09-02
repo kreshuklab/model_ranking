e5b837eb5ecf129b1fc2eb9f8e4f0f094354fe55
Joshua Talks <joshua.talks@embl.de>
2025-09-02 17:13:37 +0200

    script to run classification feature extraction

---
+++ scripts/classification/classification_feature_extraction.py
@@ -0,0 +1,77 @@
+import h5py  # pyright: ignore[reportMissingTypeStubs]
+from pathlib import Path
+import torch
+from tqdm import tqdm
+import typer
+
+from model_ranking import (
+    ClassificationPredictConfig,
+    get_classification_TTA_loaders,
+    load_checkpoint_resnet,
+    predict_with_features,
+    ResNet,
+    copy_classification_config,
+)
+
+from pytorch3dunet.unet3d.config import (
+    load_config_direct,  # pyright: ignore[reportUnknownVariableType]
+)
+
+
+def main(
+    config: Path = typer.Option(
+        ..., help="Path to config yaml for difference image calculation"
+    )
+):
+    cfg_data, _ = load_config_direct(config)
+    cfg = ClassificationPredictConfig.model_validate(cfg_data)
+
+    # get TTA dataloaders
+    loaders = get_classification_TTA_loaders(cfg.loader)
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    if device == "cpu":
+        print("WARNING: No GPU available, using CPU")
+    model = ResNet(cfg.model)
+    model = load_checkpoint_resnet(
+        cfg.model.modelname,
+        model=model,
+        path=str(cfg.model.ckpt_path),
+        location=device,
+        model_key="ResNet",
+    )
+    model = model.eval()
+
+    for aug, loader in tqdm(loaders.items()):
+        print(f"Applying {aug} Test Time Augmentation")
+        if aug == "None":
+            aug_path = "None"
+        else:
+            aug_path = Path(aug.split("_")[0]) / aug.split("_")[-1]
+        save_dir_path = Path(cfg.output.save_dir_path) / aug_path
+        # check if save path exists if not create it
+        if not save_dir_path.exists():
+            save_dir_path.mkdir(parents=True, exist_ok=True)
+
+        save_path = save_dir_path / "predictions.h5"
+
+        copy_classification_config(old_path=config, save_path=save_path)
+
+        # Run classification prediction
+        model_output = predict_with_features(
+            model,
+            loader=loader,
+            device=torch.device(device),
+            feature_layers=cfg.model.feature_layers,  # pyright: ignore
+        )
+
+        with h5py.File(save_path, "w") as f:
+            for output_key, value in model_output.items():
+                if output_key == "features":
+                    for k, v in model_output["features"].items():
+                        _ = f.create_dataset(k, data=v)
+                else:
+                    _ = f.create_dataset(output_key, data=value)
+
+
+if __name__ == "__main__":
+    typer.run(main)
