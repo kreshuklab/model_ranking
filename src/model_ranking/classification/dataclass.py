91ca07884017c6450a37f630ee1de17b372c3a07
Joshua Talks <joshua.talks@embl.de>
2025-09-02 12:02:13 +0200

    loader dataclass

--- src/model_ranking/classification/dataclass.py
+++ src/model_ranking/classification/dataclass.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from pydantic import BaseModel
-from typing import List, Literal, Optional, Union
+from typing import Any, List, Literal, Optional, Sequence, Tuple, Union
 
 
 class Conv1Config(BaseModel):
@@ -19,4 +19,41 @@ class ClassificationModelConfig(BaseModel):
     modelType: Literal["ResNet18"]
     ckpt_path: Union[str, Path]
     ckpt_key: str
-    feature_layers: Optional[List[int]]
+    feature_layers: Optional[Union[List[str], List[int]]]
+
+
+class ClassificationPatchPositionConfig(BaseModel):
+    patch_pos_path: str
+    patch_pos_key: str
+    roi: Optional[Sequence[Sequence[int]]]
+
+
+class TTAugmentationsConfig(BaseModel):
+    aug_rnd_seed: int
+    transform_params: Optional[Sequence[Sequence[Any]]]
+    raw_transform_params: Optional[Sequence[Sequence[Any]]]
+    ndim: int
+    n_samples: int
+
+
+class ClassificationFilteredDatasetConfig(BaseModel):
+    raw_path: Union[str, Path]
+    raw_key: str
+    mask_path: Union[str, Path]
+    mask_key: str
+    patch_shape: Tuple[int, int, int]
+    repeat_patches: bool
+    patch_rnd_seed: int
+    mask_return_mode: bool
+    patch_return_mode: bool
+
+
+class ClassificationTTALoaderConfig(BaseModel):
+    patch_position: ClassificationPatchPositionConfig
+    TTAugmentations: Optional[TTAugmentationsConfig]
+    dataset: ClassificationFilteredDatasetConfig
+    n_samples: int
+    ndim: int
+    batch_size: int
+    shuffle: bool
+    num_workers: int
