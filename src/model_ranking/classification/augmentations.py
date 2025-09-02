9b5670b5fe3728895d04536b3a776f68e2ff5778
Joshua Talks <joshua.talks@embl.de>
2025-09-02 17:12:27 +0200

    remove deprecated functions

--- src/model_ranking/classification/augmentations.py
+++ src/model_ranking/classification/augmentations.py
@@ -1,11 +1,7 @@
-import numpy as np
 from torch_em.segmentation import (
     get_augmentations,  # pyright: ignore[reportUnknownVariableType]
 )
-from torch_em.transform.raw import (
-    get_single_TTA_raw_augmentations,  # pyright: ignore[reportUnknownVariableType]
-)
-from typing import Any, Sequence
+from typing import Any, Dict
 
 
 from .dataclass import AugmentationsConfig
@@ -23,25 +19,8 @@ AUGMENTATION_ABBREVIATIONS = {
 }
 
 
-def classification_raw_TTAs(config: AugmentationsConfig, raw_transform: Sequence[Any]):
-    assert config.n_samples is not None, "Number of samples must be specified"
-    assert config.aug_rnd_seed is not None, "Random seed must be specified"
-    TTA_alphas = get_TTA_aug_alphas(
-        raw_transform, config.n_samples, config.aug_rnd_seed
-    )
-    TT_raw_transform = get_single_TTA_raw_augmentations(raw_transform)
-    return TTA_alphas, TT_raw_transform
-
-
 def classification_geometric_TTAs(
-    config: AugmentationsConfig, transform: Sequence[Any]
+    config: AugmentationsConfig, transform: Dict[str, Any]
 ):
     TT_transform = get_augmentations(config.ndim, transform)
     return TT_transform
-
-
-def get_TTA_aug_alphas(aug: Sequence[Any], n_samples: int, random_seed: int):
-    alpha = aug[1]["alpha"]
-    r = np.random.RandomState(random_seed)
-    alphas = r.uniform(alpha[0], alpha[1], n_samples)
-    return alphas
