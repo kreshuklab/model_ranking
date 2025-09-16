e9caf1136742f1e89ad6b51c4f5e3ed7acc89f39
Joshua Talks <joshua.talks@embl.de>
2025-09-16 16:07:18 +0200

    add function to run sequential update of batchnorm layers

--- src/model_ranking/finetuning/adaptive_batchnorm.py
+++ src/model_ranking/finetuning/adaptive_batchnorm.py
@@ -7,6 +7,7 @@ from .dataclass import AdaptiveBatchNormConfig
 from adabn.utils import (  # pyright: ignore[reportMissingTypeStubs]
     compute_bn_stats,  # pyright: ignore[reportUnknownVariableType]
     replace_bn_stats,  # pyright: ignore[reportUnknownVariableType]
+    sequential_bn_adaptation,  # pyright: ignore[reportUnknownVariableType]
 )
 from pytorch3dunet.datasets.utils import (
     get_test_loaders,  # pyright: ignore[reportUnknownVariableType]
@@ -90,4 +91,33 @@ def run_adaptive_batchnorm(config: AdaptiveBatchNormConfig):
     print("Replacing BatchNorm stats in the model...")
     replace_bn_stats(model, bn_stats)
 
+    return model
+
+
+def run_sequential_adaptive_batchnorm(config: AdaptiveBatchNormConfig):
+    model_cfg = config.model_cfg
+    model = get_model(model_cfg.model.model_dump())
+    print("Initialize from source model:", model_cfg.source_checkpoint)
+    src_ckpt_path = model_cfg.source_checkpoint
+    assert src_ckpt_path is not None, "Source checkpoint must be provided"
+    if Path(src_ckpt_path).suffix == ".pt":
+        model_key = "model_state"
+    else:
+        model_key = "model_state_dict"
+    _ = load_checkpoint(src_ckpt_path, model, model_key=model_key)
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model = model.to(device)
+    model = model.eval()
+
+    test_loaders = list(get_test_loaders(config.model_dump()))
+    assert (
+        len(test_loaders) == 1
+    ), f"Expected exactly one test loader, got {len(test_loaders)}"
+    test_loader = test_loaders[0]
+
+    print("Computing BatchNorm stats on test loader...")
+    _ = sequential_bn_adaptation(  # pyright: ignore[reportUnknownVariableType]
+        model, test_loader, verbose=False
+    )
     return model
