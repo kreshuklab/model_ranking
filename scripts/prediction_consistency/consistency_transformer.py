f06dcd07b6ae077ea4f58cceeadbccde424fad32
Joshua Talks <joshua.talks@embl.de>
2025-09-23 17:35:18 +0200

    script to find consistency for trasnformer models

---
+++ scripts/prediction_consistency/consistency_transformer.py
@@ -0,0 +1,47 @@
+from typing import Annotated
+import typer
+
+from pytorch3dunet.unet3d.config import (
+    load_config_direct,  # pyright: ignore[reportUnknownVariableType]
+)
+
+from model_ranking import (
+    run_foreground_patch_selection,
+    save_summary_metrics,
+    run_consistency_evaluation,
+    ConsistencyConfig,
+    ForegroundFilterConfig,
+    SummaryResultsConfig,
+    TransformerConsistencyMetaConfig,
+    transformer_consistency_yaml_generator,
+)
+
+
+def main(
+    config: Annotated[str, typer.Option(help="Path to the config file", exists=True)],
+):
+    cfg, _ = load_config_direct(config)
+    meta_cfg = TransformerConsistencyMetaConfig.model_validate(cfg)
+    run_config_paths = transformer_consistency_yaml_generator(meta_cfg)
+
+    for transfer_title, config_paths in run_config_paths.items():
+        print(f"Running transfer {transfer_title}")
+        for config_path in config_paths:
+            cfg, _ = load_config_direct(config_path)
+
+            if "none" in str(config_path.stem):
+                print(f"Skipping consistency evaluation for {config_path.stem}")
+            else:
+                consis_config = ConsistencyConfig.model_validate(cfg["consistency"])
+                _, _ = run_consistency_evaluation(consis_config)
+
+            # save summary metrics
+            summary_config = SummaryResultsConfig.model_validate(cfg["summary_results"])
+            if isinstance(summary_config.filter_patches, ForegroundFilterConfig):
+                _ = run_foreground_patch_selection(summary_config)
+
+            save_summary_metrics(summary_config)
+
+
+if __name__ == "__main__":
+    typer.run(main)
