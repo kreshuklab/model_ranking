668eb1b16a67aded98e6a29b3c1cb9f07cc55a3b
Joshua Talks <joshua.talks@embl.de>
2025-10-10 21:55:42 +0200

    script to recalculate segmentation from preditction boundaries

---
+++ scripts/prediction_consistency/segmentation_from_boundaries.py
@@ -0,0 +1,37 @@
+import typer
+from typing import Annotated
+
+from model_ranking import (
+    load_h5,
+    save_h5,
+    get_pred_paths,
+    calculate_segmentation,
+    CalculateSegmentationConfig,
+)
+from pytorch3dunet.unet3d.config import (
+    load_config_direct,  # pyright: ignore[reportUnknownVariableType]
+)
+
+
+def main(
+    config: Annotated[str, typer.Option(help="Path to Meta config file", exists=True)],
+):
+    cfg_values, _ = load_config_direct(config)
+    cfg = CalculateSegmentationConfig.model_validate(cfg_values)
+
+    pred_paths = get_pred_paths(cfg.pred_base_path, cfg.models)
+
+    for pred_path in pred_paths:
+        print(f"Processing {pred_path}")
+        preds = load_h5(pred_path, cfg.pred_key)
+        segs = calculate_segmentation(
+            preds,
+            min_size=cfg.min_size,
+            zero_largest_instance=cfg.zero_largest_instance,
+        )
+
+        save_h5(pred_path, cfg.output_key, segs, overwrite=cfg.overwrite_output)
+
+
+if __name__ == "__main__":
+    typer.run(main)
