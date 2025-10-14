ab8234459bb82479e47424837ad5a1a96a70dc4e
Joshua Talks <joshua.talks@embl.de>
2025-10-14 10:10:47 +0200

    run script

--- scripts/helper/visualise_napari.py
+++ scripts/helper/visualise_napari.py
@@ -42,22 +42,53 @@ def h5_napari(
 
 if __name__ == "__main__":
 
+    # h5_napari(
+    #     [
+    #         "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_Ovules_gap/consistency/P_fullslice/ov_model_NA1/norm_50_950/none/predictions/N_294_final_crop_ds2_predictions.h5",
+    #         # "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_Ovules_gap/consistency/P_fullslice/ov_model_NA1/norm_50_950/gauss_a001-005/predictions/N_294_final_crop_ds2_predictions.h5",
+    #         # "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_Ovules_gap/consistency/P_fullslice/ov_model_NA1/norm_50_950/gauss_a005-01/predictions/N_294_final_crop_ds2_predictions.h5",
+    #         "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_Ovules_gap/consistency/P_fullslice/ov_model_NA1/norm_50_950/gauss_a01-015/predictions/N_294_final_crop_ds2_predictions.h5",
+    #         "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_Ovules_gap/consistency/P_fullslice/ov_model_NA1/norm_50_950/gauss_a015-02/predictions/N_294_final_crop_ds2_predictions.h5",
+    #         "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_Ovules_gap/consistency/P_fullslice/ov_model_NA1/norm_50_950/gauss_a02-025/predictions/N_294_final_crop_ds2_predictions.h5",
+    #     ]
+    #     + ["/scratch/talks/data/Ovules/GT2x/test/N_294_final_crop_ds2.h5"] * 2,
+    #     keys=["segmentation"] * 4 + ["raw", "label"],
+    #     names=[
+    #         "seg_none",
+    #         # "seg_gauss_a001-005",
+    #         # "seg_gauss_a005-01",
+    #         "seg_gauss_a01-015",
+    #         "seg_gauss_a015-02",
+    #         "seg_gauss_a02-025",
+    #         "raw",
+    #         "label_with_ignore",
+    #     ],
+    #     are_labels=[True] * 4 + [False, True],
+    #     rois=None,
+    # )
+
     h5_napari(
         [
-            "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/FlyWing_to_Ovules_gap/consistency/P1/fw_model8/norm_5_95/none/predictions/N_294_final_crop_ds2_none.h5",
-            # "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/FlyWing_to_Ovules_gap/consistency/P1/fw_model8/norm_5_95/gauss_a005-01/predictions/N_294_final_crop_ds2_gauss_a005-01.h5",
-            "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/FlyWing_to_Ovules_gap/consistency/P1/fw_model8/norm_5_95/gauss_a01-02/predictions/N_294_final_crop_ds2_gauss_a01-02.h5",
+            "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_FlyWing_gap/consistency/P_fullslice/ov_model_UNet1/norm_50_950/none/predictions/per03_predictions.h5",
+            # "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_FlyWing_gap/consistency/P_fullslice/ov_model_UNet1/norm_50_950/gauss_a001-005/predictions/per03_predictions.h5",
+            # "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_FlyWing_gap/consistency/P_fullslice/ov_model_UNet1/norm_50_950/gauss_a005-01/predictions/per03_predictions.h5",
+            "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_FlyWing_gap/consistency/P_fullslice/ov_model_UNet1/norm_50_950/gauss_a01-015/predictions/per03_predictions.h5",
+            "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_FlyWing_gap/consistency/P_fullslice/ov_model_UNet1/norm_50_950/gauss_a015-02/predictions/per03_predictions.h5",
+            "/g/kreshuk/talks/consistency_results/Instance_segmentation/Cells/Ovules_to_FlyWing_gap/consistency/P_fullslice/ov_model_UNet1/norm_50_950/gauss_a02-025/predictions/per03_predictions.h5",
         ]
-        + ["/scratch/talks/data/Ovules/GT2x/test/N_294_final_crop_ds2_patchwise.h5"]
-        * 2,
-        keys=["segmentation"] * 2 + ["raw", "label"],
+        + ["/scratch/talks/data/FlyWing/GT/test/per03.h5"] * 2,
+        keys=["segmentation"] * 4
+        + ["volumes/raw", "volumes/labels/expanded_cells_with_ignore"],
         names=[
             "seg_none",
+            # "seg_gauss_a001-005",
             # "seg_gauss_a005-01",
-            "seg_gauss_a01-02",
+            "seg_gauss_a01-015",
+            "seg_gauss_a015-02",
+            "seg_gauss_a02-025",
             "raw",
-            "labels",
+            "label_with_ignore",
         ],
-        are_labels=[True] * 2 + [False, True],
+        are_labels=[True] * 4 + [False, True],
         rois=None,
     )
