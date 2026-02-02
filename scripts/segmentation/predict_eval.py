c70f17568fec28067857e5e9ca92151adab0d3e3
Joshua Talks <joshua.talks@embl.de>
2026-02-02 14:09:47 +0100

    remove uneeded scripts

--- scripts/segmentation/predict_eval.py
+++
@@ -1,8 +0,0 @@
-from pytorch3dunet.unet3d.config import (
-    load_config,
-)
-from model_ranking import predict_eval
-
-if __name__ == "__main__":
-    cfg, _ = load_config()
-    predict_eval(cfg)
