ca1d4681e1da4eefba10da9b00ece929b9cc3e59
Joshua Talks <joshua.talks@embl.de>
2026-02-17 13:15:15 +0100

    seperate normalisation config

--- src/model_ranking/data_structures/finetuning/data.py
+++ src/model_ranking/data_structures/finetuning/data.py
@@ -2,6 +2,12 @@ from pydantic import BaseModel
 from typing import List, Optional, Sequence, Tuple
 
 
+class DataNormalisationConfig(BaseModel):
+    global_normalisation: bool = False
+    global_percentiles: Optional[Tuple[float, float]] = None
+    norm01: bool = False
+
+
 class SelfTrainingDataConfig(BaseModel):
     unsupervised_train_paths: List[str]
     unsupervised_val_paths: List[str]
@@ -14,8 +20,7 @@ class SelfTrainingDataConfig(BaseModel):
     n_samples_val: Optional[int]
     roi_unsupervised_train: Optional[Sequence[Sequence[int]]] = None
     roi_unsupervised_val: Optional[Sequence[Sequence[int]]] = None
-    global_normalization: bool = False
-    norm01: bool = False
+    normalisation: DataNormalisationConfig
 
 
 class SupervisedDataConfig(BaseModel):
