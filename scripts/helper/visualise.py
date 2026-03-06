a40b3f539eea6c4df796ad23c93d5fd182d9a1fd
Joshua Talks <joshua.talks@embl.de>
2026-03-06 16:10:20 +0100

    epfl loading example

--- scripts/helper/visualise.py
+++ scripts/helper/visualise.py
@@ -4,16 +4,12 @@ from model_ranking.visualise import h5_napari
 if __name__ == "__main__":
 
     h5_napari(
-        [
-            "/g/kreshuk/talks/model_ranking_results/Self-Finetuning/Cells/Ovules_to_FlyWing_gap/default_selftraining/predictions/OvtoFw_def_ov_model_NA1/epoch-3/predictions/per03_predictions.h5"
-        ]
-        * 2
-        + ["/scratch/talks/data/FlyWing/GT/test/per03.h5"] * 2,
+        ["/g/kreshuk/talks/data/epfl/resized_pixels/test.h5"] * 2,
         keys=[
-            "predictions",
-            "segmentation",
+            "raw",
+            "labels",
         ],
-        names=["pred", "seg"],
+        names=["raw", "GT"],
         are_labels=[False, True],
         rois=[np.s_[50:55, :, :]] * 2,  # pyright: ignore
     )
