fe1480684207e61e46d9deefddf671ef4896565b
leazrl <lzlngstr@gmail.com>
2026-05-27 11:39:08 +0200

    change loaders type, add pactches and fg ratio

--- src/model_ranking/data_structures/finetuning/adaptive_batchnorm.py
+++ src/model_ranking/data_structures/finetuning/adaptive_batchnorm.py
@@ -3,13 +3,13 @@ from typing import Optional
 
 from .model import SelfTrainingModelConfig
 
-from model_ranking.data_structures.data import test_loaders_type
+from model_ranking.data_structures.data import val_loaders_type
 
 
 class AdaptiveBatchNormConfig(BaseModel):
     model_cfg: SelfTrainingModelConfig
-    loaders: test_loaders_type
+    loaders: val_loaders_type
     output_checkpoint_dir_path: str
-    data_fraction: Optional[float] = None
+    n_patches: Optional[int] = None
     foreground_ratio_threshold: Optional[float] = 0.0
 
