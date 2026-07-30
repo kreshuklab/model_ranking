16a4ca8e8b8c4d4fd8e60634a5cbdce4eea087da
leazrl <lzlngstr@gmail.com>
2026-07-30 14:07:52 +0200

    exclude Blackwel GPUS

--- scripts/finetuning/update_batch_norm.sh
+++ scripts/finetuning/update_batch_norm.sh
@@ -71,7 +71,7 @@ for config_file in "${config_files[@]}"; do
 #SBATCH --mail-type=FAIL		            # specify mail notifications for your job 
 #SBATCH --mail-user=lea.zur_lage@embl.de    # specify the mail address for mail notifications 
 #SBATCH -p gpu				                    # specify the queue you want to submit to; here we choose the gpu queue. If you want to submit a pure CPU job, just leave this out.
-#SBATCH --exclude=gpu[50-53],bgx1                # exclude Blackwell GPUs
+#SBATCH --exclude=gpu[50-53],bgx1               # exclude Blackwell GPUs
 #SBATCH --gres=gpu:1			                # specify the number of gpus per node
 
 # next we should load all the modules we need to run the job.
